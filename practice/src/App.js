edb8a3c359a9c4c3077593a0cd22ef344119c502
선종헌 <acer1052@naver.com>
2022-01-17 10:06:23 +0900

    0001. Header

--- practice/src/App.js
+++ practice/src/App.js
@@ -1,11 +1,15 @@
 /*eslint-disable*/
 import './App.css';
+import Header from './Header';
 
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+	return (
+	<div className="App">
+		<div className="App_wrap">
+			<Header />
+		</div>
+	</div>
+	);
 }
 
 export default App;
---
+++ practice/src/Header.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import './Header.css';
+
+function Header() {
+    const login = () => {
+        alert('Hello');
+    };
+
+    const loginKey = (event) => {
+        if (event.keyCode === 13){
+            login();
+        };
+    };
+
+    return(
+        <div className="Header">
+            <div className="Header_wrap">
+                <a href="#" className="Header__logo">React</a>
+                <div className="Header__submit" onSubmit={login}>
+                    <div className="Header__input_wrap">
+                        <input type="text" className="Header__input_box" placeholder="ID" onKeyUp={loginKey} />
+                        <input type="password" className="Header__input_box" placeholder="PW" onKeyUp={loginKey} />
+                    </div>
+                    <div className="Header__button_wrap">
+                        <button className="Header__button">login</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
